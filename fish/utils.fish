f9afc5d85b21cbd581cdcd4c14a0de8065c83865
inigo.hidalgorey <inigohrey@gmail.com>
2025-07-29 10:32:51 +0000

    fix: escape hyperlink

--- fish/utils.fish
+++ fish/utils.fish
@@ -13,5 +13,5 @@ function create_hyperlink --description 'Create a clickable hyperlink for termin
     set -l display_text $argv[2]
     
     # Return the formatted hyperlink string using ANSI escape sequences
-    printf "%b" "\033]8;;$url\007$display_text\033]8;;"
+    printf "%b" "\033]8;;$url\007$display_text\033]8;;\007"
 end
