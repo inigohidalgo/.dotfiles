439f0bc353b5744ba35b4edfeb45a354b5958ae5
inigo.hidalgorey <inigohrey@gmail.com>
2025-09-18 14:57:37 +0000

    feat(fish/nav): add concrete tool navigation dirs

--- fish/nav.fish
+++ fish/nav.fish
@@ -17,4 +17,8 @@ abbr -a --set-cursor cdrw 'cd $GWT_DIR/%'
 abbr -a --set-cursor cdri 'cd $REPOS_DIR/ihr/%'
 
 abbr -a --set-cursor cdt 'cd $TOOLS_DIR/%'
+abbr -a --set-cursor cdtp 'cd $TOOLS_DIR/pythia/%'
+abbr -a --set-cursor cdtb 'cd $TOOLS_DIR/bimo/%'
+abbr -a --set-cursor cdtd 'cd $TOOLS_DIR/dsbuilder/%'
+
 abbr -a --set-cursor cdu 'cd $UC_DIR/%'
