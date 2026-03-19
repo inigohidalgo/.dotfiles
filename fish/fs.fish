56d5987060e0ef4355e50fee7ad831eb13a8c5ce
Inigo Hidalgo Rey <inigohrey@gmail.com>
2026-03-19 10:21:22 -0700

    better mkd

--- fish/fs.fish
+++ fish/fs.fish
@@ -1,5 +1,5 @@
 function mkd --description 'Create directory with date or UUID prefix'
-    argparse 'h/help' 'u/uuid' 'p/path=' 'j/jump' -- $argv
+    argparse 'h/help' 'u/uuid' 'p/path=' 'j/jump' 'e/editor' 'c/chat' 't/tmp' 's/stacks' -- $argv
     or return 1
 
     if set -q _flag_help; or test (count $argv) -eq 0
@@ -12,12 +12,17 @@ function mkd --description 'Create directory with date or UUID prefix'
         echo "  -u, --uuid       Use UUID prefix instead of date" >&2
         echo "  -p, --path PATH  Create directory in PATH (default: current dir)" >&2
         echo "  -j, --jump       cd into the created directory" >&2
+        echo "  -e, --editor     Open the directory in VS Code" >&2
+        echo "  -c, --chat       Create in ~/chat/" >&2
+        echo "  -t, --tmp        Create in ~/dev/tmp/" >&2
+        echo "  -s, --stacks     Create in ~/dev/stacks/" >&2
         echo "" >&2
         echo "Examples:" >&2
         echo "  mkd my-project          # Creates 2024-01-31_my-project" >&2
         echo "  mkd -u temp             # Creates 550e8400-e29b_temp" >&2
         echo "  mkd -j work             # Creates dir and cd's into it" >&2
-        echo "  mkd foo | code          # Creates dir and opens in VS Code" >&2
+        echo "  mkd -cj my-chat         # Creates in ~/chat/ and cd's into it" >&2
+        echo "  mkd -e foo              # Creates dir and opens in VS Code" >&2
         echo "" >&2
         echo "Sets \$created_dir to the created path for scripting." >&2
         return 0
@@ -40,8 +45,25 @@ function mkd --description 'Create directory with date or UUID prefix'
     set -l dir_name "$prefix"_"$sanitized_desc"
 
     # Determine base path
+    set -l location_flags 0
+    set -q _flag_chat; and set location_flags (math $location_flags + 1)
+    set -q _flag_tmp; and set location_flags (math $location_flags + 1)
+    set -q _flag_stacks; and set location_flags (math $location_flags + 1)
+    set -q _flag_path; and set location_flags (math $location_flags + 1)
+
+    if test $location_flags -gt 1
+        echo "Error: -c, -t, -s, and -p are mutually exclusive" >&2
+        return 1
+    end
+
     set -l base_path
-    if test -n "$_flag_path"
+    if set -q _flag_chat
+        set base_path ~/chat
+    else if set -q _flag_tmp
+        set base_path ~/dev/tmp
+    else if set -q _flag_stacks
+        set base_path ~/dev/stacks
+    else if test -n "$_flag_path"
         set base_path "$_flag_path"
     else
         set base_path "$PWD"
@@ -57,6 +79,10 @@ function mkd --description 'Create directory with date or UUID prefix'
         set -g created_dir "$full_path"
         echo "Variable \$created_dir set to: $full_path" >&2
         echo "$full_path"
+        if set -q _flag_editor
+            echo "Opening in VS Code: $full_path" >&2
+            code "$full_path"
+        end
         if set -q _flag_jump
             echo "Changing directory to: $full_path" >&2
             cd "$full_path"
